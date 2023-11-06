baf6365d1758bcfe7bf06f4b2f38e08f77e2cffd
Yamazaki Takuo <2155085@stu.hus.ac.jp>
2023-11-06 19:15:42 +0900

    URLの変更戻し

--- src/Pages/Top.jsx
+++ src/Pages/Top.jsx
@@ -16,7 +16,7 @@ export const Top = () => {
 
     const fetchTasks = async () => {
         try {
-          const response = await axios.get(`${apiUrl}`);
+          const response = await axios.get(`${apiUrl}/api/tasks`);
           setTasks(response.data);
         } catch (error) {
           console.error('Tasksの取得エラー:', error);
