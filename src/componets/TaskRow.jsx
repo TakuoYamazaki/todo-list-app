762dda81602effea373c4c222f9a717e124109da
Yamazaki Takuo <2155085@stu.hus.ac.jp>
2023-11-18 10:41:26 +0900

    完成版

--- src/componets/TaskRow.jsx
+++ src/componets/TaskRow.jsx
@@ -3,6 +3,48 @@ import { Link } from 'react-router-dom';
 
 function TaskRow({ task, onDelete, onPut}) {
 
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  const handlePut = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${apiUrl}${task.id}/complete/`, {
+        method: 'PUT',
+      });
+
+      // レスポンスの処理などを行う
+      if (response.ok) {
+        // 成功時の処理
+        window.location.reload();
+      } else {
+        // エラー時の処理
+      }
+    } catch (error) {
+      console.error('PUT リクエストエラー:', error);
+    }
+  };
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${apiUrl}${task.id}/`, {
+        method: 'DELETE',
+      });
+
+      // レスポンスの処理などを行う
+      if (response.ok) {
+        // 成功時の処理
+        window.location.reload();
+      } else {
+        // エラー時の処理
+      }
+    } catch (error) {
+      console.error('DELETE リクエストエラー:', error);
+    }
+  };
+
   return (
     <tr key={task.id}>
       <td className="px-3 py-4 text-sm text-gray-500">{task.name}</td>
@@ -10,13 +52,12 @@ function TaskRow({ task, onDelete, onPut}) {
         <div className="flex justify-end">
           <div>
             <form
-              onSubmit={onPut}
+              onSubmit={handlePut}
               method="put"
               className="inline-block text-gray-500 font-medium"
               role="menuitem"
               tabIndex="-1"
             >
-              <input type="hidden" name="id" value={task.id} />
               <button
                 type="submit"
                 className="bg-emerald-700 py-4 w-20 text-white md:hover:bg-emerald-800 transition-colors"
@@ -27,7 +68,7 @@ function TaskRow({ task, onDelete, onPut}) {
           </div>
           <div>
             <Link
-              to={`/tasks/${task.id}/edit`}
+              to={`/tasks/${task.id}/edit/`}
               className="inline-block text-center py-4 w-20 underline underline-offset-2 text-sky-600 md:hover:bg-sky-100 transition-colors"
             >
               編集
@@ -35,13 +76,13 @@ function TaskRow({ task, onDelete, onPut}) {
           </div>
           <div>
             <form
-              onSubmit={onDelete}
+              onSubmit={handleDelete}
+              action={`${apiUrl}${task.id}/`}
               method="delete"
               className="inline-block text-gray-500 font-medium"
               role="menuitem"
               tabIndex="-1"
             >
-              {/* <input type="hidden" name="id" value={task.id} /> */}
               <button
                 type="submit"
                 className="py-4 w-20 md:hover:bg-slate-200 transition-colors"
